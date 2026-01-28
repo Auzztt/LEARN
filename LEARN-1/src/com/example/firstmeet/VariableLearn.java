64b5838df5a9420d14f71d85459e56bb92a82e78
Auzztt <zhangxx503@163.com>
2026-01-28 18:58:41 +0800

    学习键盘录入、算数运算符、关系运算符、逻辑运算符、三元运算符

--- LEARN-1/src/com/example/firstmeet/VariableLearn.java
+++ LEARN-1/src/com/example/firstmeet/VariableLearn.java
@@ -1,5 +1,7 @@
 package com.example.firstmeet;
 
+import java.util.Scanner;
+
 public class VariableLearn {
     public static void main(String[] args){
         /* 微信余额：100元
@@ -31,5 +33,51 @@ public class VariableLearn {
         a = a + 10;
         a = a - 2 ;
         System.out.println(a);
+
+        /*
+            算数运算符：+ - * / %
+
+            整数计算、小数计算
+        */
+        // 1. 整数计算
+        // 细节：整数相除结果还是整数，就是商
+        //        % 取模，取余
+        //       其他运算跟数学中是一模一样的
+        // 2. 小数计算
+        //       小数直接参与计算，结果有可能不精确
+        double d = 1.1;
+        double e = 1.01;
+
+        System.out.println(d + e);
+        System.out.println(d - e);
+        System.out.println(d * e);
+        System.out.println(d / e);
+        System.out.println(d % e);
+
+        //可以利用取模来判断一个数是奇数还是偶数
+        System.out.println(15 % 2); //1  结果非0就是奇数
+
+        // 数值拆分练习
+        //1.键盘录入一个三位数
+        //导包 --- 创建对象 --- 接收数据
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个三位整数");
+        int number = sc.nextInt(); //123
+
+        //2.获取这个三位数的个位、十位、百位并打印出来
+        //公式：
+        //针对于任意的一个数而言
+        //个位： 数字 % 10
+        int ones = number % 10;
+        //十位： 数字 / 10 % 10
+        int tens = number / 10 % 10;
+        //百位： 数字 / 100 % 10
+        int hundreds = number / 100  % 10;
+
+        //输出结果
+        System.out.println("该三位数的个位、十位、百位分别为：");
+        System.out.println(ones);
+        System.out.println(tens);
+        System.out.println(hundreds);
     }
 }
